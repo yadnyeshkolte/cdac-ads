55b534a88d1a06c14d6f9eedc0ea46bef0fa18b6
Yadnyesh <yadnyeshkolte@gmail.com>
2025-09-30 18:29:49 +0530

    update

--- adsLabDay1/Q1.java
+++ adsLabDay1/Q1.java
@@ -1,13 +1,12 @@
-```java
-
-public class Hello {
+package adsLabDay1;
+import java.util.Scanner;
+public class Q1 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Please Enter the String 1: ");
 		String s1 = scan.next();
 		System.out.print("Please Enter the String 2: ");
 		String s2 = scan.next();
-		System.out.println("");
 		
 	   String[][] matrix = new String[s1.length()][s2.length()];
 	   for (int i = 0; i < matrix.length; i++) {
@@ -23,7 +22,7 @@ public class Hello {
 	   for(int i=0;i<s1.length();i++) {
 		   for(int j=0;j<s2.length();j++) {
 			   if(s1.charAt(i)==s2.charAt(j)) {
-				   indexr = i; //0
+				   indexr = i;
 				   indexc = j; 
 				   bool = true;
 				   breakbool = true;
@@ -34,27 +33,25 @@ public class Hello {
 			   break;
 		   }
 	   }
-	   System.out.println(indexr);
-	   System.out.println(indexc);
 	   if(bool == false) {
-		   System.out.println("Cannot have similer");
+		   System.out.println("Donot have similer Characters");
 	   }
 	   
-	   for(int i=0;i<s1.length();i++) {
-		   matrix[i][indexc] = s1.charAt(i)+"";
+	   if(bool) {
+		   for(int i=0;i<s1.length();i++) {
+			   matrix[i][indexc] = s1.charAt(i)+"";
+		   }
+		   
+		   for(int i=0;i<s2.length();i++) {
+			   matrix[indexr][i] = s2.charAt(i)+"";
+		   }
+		   // to show the matrix in proper way 
+		   for (int i = 0; i < matrix.length; i++) {
+	           for (int j = 0; j < matrix[i].length; j++) {
+	               System.out.print(matrix[i][j] + " ");
+	           }
+	           System.out.println();
+	       }
 	   }
-	   
-	   for(int i=0;i<s2.length();i++) {
-		   matrix[indexr][i] = s2.charAt(i)+"";
-	   }
-
-	   for (int i = 0; i < matrix.length; i++) {
-           for (int j = 0; j < matrix[i].length; j++) {
-               System.out.print(matrix[i][j] + " ");
-           }
-           System.out.println(); // Move to next line after each row
-       }
 	}
 }
-
-```
